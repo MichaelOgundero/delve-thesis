261d6fdef01ba1cc853f2f2bd5ce81ca7971a38f
youngTickles <ogunderomichael@gmail.com>
2019-11-09 02:00:25 +0100

    made the sign up page (subject to change)

--- app/src/signup/AppSignUp.js
+++ app/src/signup/AppSignUp.js
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
 import { NavLink as Link} from 'react-router-dom';
+
 import AppFooter from '../footer/AppFooter.js'
 import "./AppSignUp.css";
+import cinemaChair from '../images/cinemaChair.jpg';
+import facebookIcon from '../images/facebookIcon.png';
+import googleIcon from '../images/googleIcon.png';
 
 import {
     Collapse,
@@ -12,7 +16,7 @@ import {
     NavItem,
      NavLink,
      Row,
-     Container, Col
+     Container, Col, Button, Form, FormGroup, Label, Input
    } from 'reactstrap';
 
 
@@ -49,17 +53,55 @@ class AppSignUp extends Component{
             {/*Navigation*/}
                 <Row style={{paddingTop:"20px"}}>
                 
-                    <div style={{border:"1px solid black", height:"700px", width:"700px", position:"absolute",
+                    <div style={{ background:"black", height:"600px", width:"700px", position:"absolute",
                                  left:"0", right:"0", top:"0", bottom:"0", margin:"auto",
-                                 maxHeight:"100%", maxWidth:"100%", overflow:"auto"}}>
-                        <div style={{display:"inline-block", position:"relative", width:"300px", height:"650px", border:"1px solid red", marginLeft:"20px", marginTop:"25px"}}>
-                            <div style={{position: "absolute", bottom: "0px", width: "100%", height: "100px", border: "1px solid green"}}>
-                                <NavLink tag={Link} activeClassName="current" exact to="/signIn">
-                                    <p>I'm already a member</p>
-                                </NavLink>   
-                            </div>                            
+                                 maxHeight:"100%", maxWidth:"100%"}}>
+                        <div style={{display:"inline-block", position:"relative", width:"300px", background:"black", height:"550px", marginLeft:"20px", marginTop:"25px"}}>
+                            <div style={{maxWidth:"300px", maxHeight:"198.7px", marginTop:"108px"}}>
+                                <img src={cinemaChair}   style={{maxWidth:"300px", maxHeight:"198.7px"}}></img>
+                            </div> 
+                            <div style={{marginLeft:"50px",marginTop:"60px"}}>
+                                <NavLink tag={Link} activeClassName="current" exact to="/signIn" style={{margin:"0", padding:"0"}}>
+                                    <p style={{color:"white"}}>I'm already a member</p>
+                                </NavLink>  
+                            </div>                          
+                        </div>
+                        <div style={{position: "absolute", display:"inline-block", width:"350px", height:"550px",marginLeft:"10px", marginTop:"25px", background:"black"}}>
+                            <div style={{ marginTop:"10px", marginLeft:"10px", marginRight:"10px"}}>
+                                <p style={{color:"#fec106", fontWeight:"bold", fontSize:"32px"}}>Sign Up</p>
                             </div>
-                        <div style={{display:"inline-block", width:"350px", height:"650px", border:"1px solid blue",marginLeft:"10px", marginTop:"25px"}}>
+                            <div style={{ marginTop:"50px", marginLeft:"10px", marginRight:"10px"}}>
+                                <Form>
+                                    <FormGroup>
+                                        <Input style={{background:"black", border:"1px solid #fec106", color:"white"}} type="name" placeholder="Username" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input style={{background:"black", border:"1px solid #fec106", color:"white"}} type="email" name="email" id="exampleEmail" placeholder="Email@Example.com" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input style={{background:"black", border:"1px solid #fec106", color:"white"}} type="password" name="password" id="examplePassword" placeholder="Password" />
+                                    </FormGroup>
+                                    <FormGroup>
+                                        <Input style={{background:"black", border:"1px solid #fec106", color:"white"}}  name="password" id="examplePassword" placeholder="Repeat Password" />
+                                    </FormGroup>
+                                    <div style={{marginTop:"50px"}}>
+                                        <Button color="warning" size="lg">Register</Button>
+                                    </div>
+                                </Form>
+                            </div>
+                            
+                            <div style={{marginTop:"50px", marginLeft:"10px", marginRight:"10px"}}>
+                                <div style={{display:"inline-block"}}>
+                                    <p style={{color:"white", fontWeight:"bold", fontSize:"15px"}}>Or Login with</p>
+                                </div>
+                                <div style={{display:"inline-block", background:"white", padding:"0", margin:"0", marginLeft:"15px"}}>
+                                    <img src={facebookIcon} style={{height:"50px", width:"50px"}}></img>
+                                </div>
+                                <div style={{display:"inline-block", background:"white", padding:"0", margin:"0", marginLeft:"15px"}}>
+                                    <img src={googleIcon} style={{height:"50px", width:"50px"}}></img>
+                                </div>
+                            </div>
+
 
                         </div>
                     </div>
